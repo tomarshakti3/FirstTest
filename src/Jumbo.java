87e649fa5e0a439cabd073dc80c931f7a2431df3
tomarshakti3 <tomarshakti3@gmail.com>
2017-11-13 13:31:15 +0000

    initial commit

---
+++ src/Jumbo.java
@@ -0,0 +1,9 @@
+
+public class Jumbo {
+    public String hh;
+    public int kin;
+    public static void main(String[] args){
+      System.out.println("Jumbo");  
+    }
+    
+}
---
+++ src/MyBean.java
@@ -0,0 +1,18 @@
+
+public class MyBean implements Runnable {
+    public String stringstr;
+    public void myBeanPrint(){
+        System.out.println("inside mybeanPrint");
+    }
+    public void execute(){
+        
+    }
+    
+@Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }    
+    
+}
+
